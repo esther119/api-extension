bae88edfaa5d5480b55cfbabb17fbb7c1d10f53f
swimmingcircle119 <lampmaa22@gmail.com>
2024-01-07 22:39:41 -0800

    in the middle of adding a button

--- sw-omnibox.js
+++ sw-omnibox.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
     console.log("chrome.runtime.Reason", reason);
     if (reason === "update") {
       chrome.storage.local.set({
-        apiSuggestions: ["tabs", "storage", "scripting"],
+        apiSuggestions: ["Background_Fetch_API", "Document_Object_Model", "Media_Capture_and_Streams_API"],
       }, () => {
         // Retrieve the apiSuggestions and log it
         chrome.storage.local.get("apiSuggestions", (result) => {
@@ -45,7 +45,11 @@ chrome.omnibox.onInputEntered.addListener((input) => {
 // Save to local storage
 async function updateHistory(input) {
   const { apiSuggestions } = await chrome.storage.local.get("apiSuggestions");
+  
+  // Add the new 'input' to the beginning of the 'apiSuggestions' array
   apiSuggestions.unshift(input);
+    
+  // Remove elements from the end of 'apiSuggestions' if it exceeds 'NUMBER_OF_PREVIOUS_SEARCHES'
   apiSuggestions.splice(NUMBER_OF_PREVIOUS_SEARCHES);
   console.log("apiSuggestions updateHistory", apiSuggestions);
   console.log("number of previous searches", NUMBER_OF_PREVIOUS_SEARCHES)
--- sw-tips.js
+++ sw-tips.js
@@ -1,1 +1,54 @@
-console.log("sw-tips.js")
+console.log("sw-tips.js");
+
+// Fetch tip & save in storage
+const updateTip = async () => {
+  console.log("updateTip function called")
+  const response = await fetch("https://extension-tips.glitch.me/tips.json");
+  const tips = await response.json();
+  console.log("tips", tips);
+  const randomIndex = Math.floor(Math.random() * tips.length);
+  console.log("tip index", tips[randomIndex]);
+  return chrome.storage.local.set({ tip: tips[randomIndex] });
+};
+
+const ALARM_NAME = "tip";
+
+// Check if alarm exists to avoid resetting the timer.
+// The alarm might be removed when the browser session restarts.
+async function createAlarm() {
+  const alarm = await chrome.alarms.get(ALARM_NAME);
+  console.log("create alarm", alarm);
+  if (typeof alarm === "undefined") {
+    chrome.alarms.create(ALARM_NAME, {
+      delayInMinutes: 1,
+      periodInMinutes: 1440, //create a daily alarm (every 24 hours)
+    });
+    updateTip();
+  }
+}
+
+createAlarm();
+
+// Update tip once a day
+chrome.alarms.onAlarm.addListener(updateTip);
+// When any alarm triggers, the function updateTip is called.
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting === "tip") {
+    chrome.storage.local.get("tip").then(sendResponse); // random output from the https://extension-tips.glitch.me/tips.json 
+    console.log("send tip to service worker"); // sendResponse is a function here, not the response data itself.
+    return true;
+  }
+});
+
+// log tip
+async function logTip() {
+  try {
+    const data = await chrome.storage.local.get("tip");
+    console.log("Log Tip:", data.tip);
+  } catch (error) {
+    console.error("Error retrieving tip:", error);
+  }
+}
+
+logTip();
